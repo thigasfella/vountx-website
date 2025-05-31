6e2c0a0df18d3f1650dd311f7701cb6dc625f0b6
Thiago Lopes de Souza <thiagolpssouza@gmail.com>
2025-05-31 00:57:51 -0300

    fea - SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -6,8 +7,20 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
 export const metadata: Metadata = {
-  title: "VountX",
-  description: "A software house que seu negócio precisa!",
+  title: "VountX - Software House",
+  description:
+    "A VountX é uma software house especializada em soluções práticas, escaláveis e sob medida. Leve seu negócio ao próximo nível com a nossa expertise.",
+  robots: "index, follow",
+  openGraph: {
+    title: "VountX - Conectando negócios à inovação",
+    description:
+      "Soluções tecnológicas que impulsionam seu crescimento com eficiência e inovação.",
+    url: "https://www.vountx.com.br",
+    siteName: "VountX",
+    locale: "pt_BR",
+    type: "website",
+  },
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
@@ -18,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body id="all-content">
-          {children}
+        {children}
       </body>
     </html>
   );
